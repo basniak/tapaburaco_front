563e0140ae17ad61daf9330a361872b724cec1bc
Vinicius Basniak <vinicius.basniak@gmail.com>
2019-11-27 22:50:54 -0300

    configuração do node modules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC-0uZx9FFNlbbYDwQbl_OOIJuBVd-mbGQ",
+    authDomain: "tapaburacobeta.firebaseapp.com",
+    databaseURL: "https://tapaburacobeta.firebaseio.com",
+    projectId: "tapaburacobeta",
+    storageBucket: "tapaburacobeta.appspot.com",
+    messagingSenderId: "439678358593",
+    appId: "1:439678358593:web:67838d1c5a4c785003a2c0",
+    measurementId: "G-DJM2JFCFP7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC-0uZx9FFNlbbYDwQbl_OOIJuBVd-mbGQ",
+    authDomain: "tapaburacobeta.firebaseapp.com",
+    databaseURL: "https://tapaburacobeta.firebaseio.com",
+    projectId: "tapaburacobeta",
+    storageBucket: "tapaburacobeta.appspot.com",
+    messagingSenderId: "439678358593",
+    appId: "1:439678358593:web:67838d1c5a4c785003a2c0",
+    measurementId: "G-DJM2JFCFP7"
+  }
 };
 
 /*
